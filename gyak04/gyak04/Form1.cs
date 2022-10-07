9948dee96c969616a054fde525bbe1a7fa18f16e
Krisi <kristof.sarkozi@stud.uni-corvinus.hu>
2022-10-07 10:38:51 +0200

    Excel open(6-ig)

--- gyak04/gyak04/Form1.cs
+++ gyak04/gyak04/Form1.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace gyak04
 {
     public partial class Form1 : Form
     {
+        List<Flat> flat_list;
+        RealEstateEntities context = new RealEstateEntities();
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            CreateExcel();
+        }
+
+        private void LoadData()
+        {
+            flat_list = context.Flat.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string error = string.Format("ERROR: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(error, "ERROR");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 }
